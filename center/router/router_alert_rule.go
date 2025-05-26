4088a4ab3e7ee439205d1af8149b75102cac73df
smx_Moragn <1048492656@qq.com>
2025-05-26 20:54:50 +1000

    通知配置 & 生效配置 v2

--- center/router/router_alert_rule.go
+++ center/router/router_alert_rule.go
@@ -250,8 +250,7 @@ func (rt *Router) alertRuleEnableTryRun(c *gin.Context) {
 	if f.AlertRuleConfig.EnableInBG == 1 {
 		// 判断事件是否在业务组下
 		if curEvent.GroupId != f.AlertRuleConfig.GroupId {
-			ginx.Bomb(http.StatusBadRequest, "The event does not belong to the current business group")
-			return
+			ginx.Dangerous(errors.New("The event does not belong to the current business group"))
 		}
 	}
 	// 判断事件发生时间是否满足enable_days_of_weeks,enable_stimes,enable_etimes 条件
